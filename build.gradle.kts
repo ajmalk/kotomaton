5bdd6adc7a30e1c5caadb6e25d517d2a9af7cf9c
Ajmal Kunnummal <ajmal@gatech.edu>
2023-09-17 16:57:43 -0700

    Update Detekt configuration
     - Moved to a Gradle based workflow
     - Added .editorconfig and custom config file for Detekt
     - Turn on libraries rule set for safer binary compatibly
     - Also cleaned up some of the lints from previously committed files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,69 @@
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+  kotlin("multiplatform") version "1.9.0"
+  id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 
-group = "me.ajmalk"
-version = "1.0-SNAPSHOT"
+group = "com.kotomaton"
 
-repositories {
-    mavenCentral()
+version = "0.01"
+
+repositories { mavenCentral() }
+
+detekt {
+  toolVersion = "1.23.1"
+  source.setFrom(
+      "src/commonMain/kotlin",
+      "src/commonTest/kotlin",
+      "build.gradle.kts",
+      "settings.gradle.kts",
+  )
+
+  basePath = projectDir.absolutePath
+  config.setFrom(file("config/detekt/detekt.yml"))
+  buildUponDefaultConfig = true
+
+  dependencies { detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:1.23.1") }
 }
 
+@Suppress("UnusedPrivateProperty")
 kotlin {
-    jvm {
-        jvmToolchain(17)
-        withJava()
-        testRuns.named("test") {
-            executionTask.configure {
-                useJUnitPlatform()
-            }
-        }
-    }
-    js {
-        browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled.set(true)
-                }
-            }
-        }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+  explicitApi()
 
-    
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+  jvm {
+    jvmToolchain(17)
+    withJava()
+    testRuns.named("test") { executionTask.configure { useJUnitPlatform() } }
+  }
+
+  js { browser() }
+
+  val hostOs = System.getProperty("os.name")
+  val isArm64 = System.getProperty("os.arch") == "aarch64"
+  val isMingwX64 = hostOs.startsWith("Windows")
+  when {
+    hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+    hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+    hostOs == "Linux" && isArm64 -> linuxArm64("native")
+    hostOs == "Linux" && !isArm64 -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies { implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+      }
+    }
+    val jvmMain by getting
+    val jvmTest by getting
+    val jsMain by getting
+    val jsTest by getting
+    val nativeMain by getting
+    val nativeTest by getting
+  }
 }
